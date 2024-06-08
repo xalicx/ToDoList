49d74dadbf349a9153f34973cca606f187a66aa2
ALICJA\alicj <alicjapiotrowicz123@gmail.com>
2024-06-08 11:50:35 +0200

    list

--- simple-todo-list/src/App.js
+++ simple-todo-list/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Todo List</h1>
+        <input 
+          type="text" 
+          value={inputValue} 
+          onChange={handleInputChange} 
+          placeholder="Add a new todo" 
+        />
+        <button onClick={handleAddTodo}>Add</button>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
